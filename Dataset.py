d8c1cfe1a96f59c7215696d74458e5a687dbecf1
ritu-thombre99 <renitath99@gmail.com>
2023-11-02 17:29:49 -0700

    test 127 ibm_brisbane

--- Dataset.py
+++ Dataset.py
@@ -188,8 +188,8 @@ for it in range(iteration[0]):
     # data = datetime.date(2023, month, day)
     data = datetime.datetime.today() - datetime.timedelta(days=random.randint(1,150))
     n_qs = [2,3,4,5]
-    #backend_name_1 = [('ibm_brisbane',127)]
-    backend_name_1 = [('ibm_lagos',7),('ibm_perth',7),('ibm_nairobi',7)]
+    backend_name_1 = [('ibm_brisbane',127)]
+    # backend_name_1 = [('ibm_lagos',7),('ibm_perth',7),('ibm_nairobi',7)]
     # file_name = '/home/ritu/DNN_for_Qubit_Mapping/dataset/dataset_tesi/Dataset_Prova_4_08.csv'
     file_name = 'dataset/dataset_tesi/NN1_Dataset(<=10Cx)_balanced1.csv'
     print(data)
@@ -198,8 +198,11 @@ for it in range(iteration[0]):
     for _ in range(2):
         for backend in backend_name_1:
             backend_name, n_qubits = backend
-            update_csv(file_name, backend_name, rows_to_add=1, random_n_qubit=n_qubits, random_depth=2, min_n_qubit=n_qubits, datatime=data, show=True)
-
+            if backend_name != 'ibm_brisbane':
+                update_csv(file_name, backend_name, rows_to_add=1, random_n_qubit=n_qubits, random_depth=2, min_n_qubit=n_qubits//2, datatime=data, show=True)
+            else:
+                update_csv(file_name, backend_name, rows_to_add=1, random_n_qubit=20, random_depth=2, min_n_qubit=9, datatime=data, show=True)
+                
 
 
 
