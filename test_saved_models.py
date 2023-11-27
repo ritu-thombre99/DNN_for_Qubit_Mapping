9458210c7154b358cfd3bcb1ba8804df06054cba
ritu-thombre99 <renitath99@gmail.com>
2023-11-27 07:09:02 -0800

    did not sleep

--- test_saved_models.py
+++ test_saved_models.py
@@ -1,8 +1,5 @@
 from get_test_datasets import return_dataset
 test_dataset = return_dataset()
-
-len(test_dataset)
-
 import tensorflow as tf
 from tensorflow import keras
 
@@ -13,6 +10,29 @@ mlp_edge_feature = tf.keras.models.load_model("models/mlp_edge_features.keras")
 dnn = tf.keras.models.load_model("models/dnn.keras")
 mlp = tf.keras.models.load_model("models/mlp.keras")
 
+basis_gate_dict = {}
+provider = IBMQ.get_provider(hub='ibm-q')
+provider.backends(simulator=False)
+backends = ['ibm_lagos','ibm_perth','ibm_nairobi']
+for backend_name in backends:
+    backend = provider.get_backend(backend_name)
+    basis_gats = backend.configuration().basis_gates
+    basis_gate_dict[backend_name] = basis_gats
+
+
+def get_transpiled_circ_results(qc, backend, initial_layout):
+    basis_gats = basis_gate_dict[backend]
+    pass_ = Unroller(basis_gats)
+    pm = PassManager(pass_)
+    backend = provider.get_backend(backend)
+    
+    tqc = transpile(qc, backend=backend,initial_layout=initial_layout, optimization_level=0,seed_transpiler=13)
+    tqc = pm.run(tqc)
+    gates = dict(tqc.count_ops())
+    return gates['cx'], tqc.depth()
+
+
+
 
 SS = StandardScaler()
 num_qubits = 7
@@ -20,7 +40,12 @@ count_or = 0
 count_nr = 0
 count_mlp = 0
 count_mlp_edge = 0
-for qc_i in tqdm(range(len(test_dataset[:100]))):
+original_depth, original_CNOT = [],[]
+mlp_edge_depth, mlp_edge_CNOT = [],[]
+dnn_without_ro_depth,dnn_without_ro_CNOT = [],[] 
+dnn_with_ro_depth,dnn_with_ro_CNOT = [],[] 
+mlp_depth, mlp_CNOT = [],[]
+for qc_i in tqdm(range(len(test_dataset))):
     qc = test_dataset[qc_i]
     data = datetime.today() - timedelta(days=random.randint(1,150))
     backends = ['ibm_lagos','ibm_perth','ibm_nairobi']
@@ -58,9 +83,64 @@ for qc_i in tqdm(range(len(test_dataset[:100]))):
     print(mlp_edges_pred)
     if mlp_edges_pred == labels:
         count_mlp_edge = count_mlp_edge + 1
+    
+    
+    # original circuit after unrolling
+    cx, depth = get_transpiled_circ_results(qc, backend, initial_layout=labels)
+    original_depth.append(depth)
+    original_CNOT.append(cx)
+    print(original_depth[-1],original_CNOT[-1])
+    if len(mlp_edges_pred) == len(set(mlp_edges_pred)):
+        cx, depth = get_transpiled_circ_results(qc, backend, initial_layout=mlp_edges_pred)
+        mlp_edge_depth.append(depth)
+        mlp_edge_CNOT.append(cx)
+    else:
+        mlp_edge_depth.append(float('NaN'))
+        mlp_edge_CNOT.append(float('NaN'))
         
+    # NN without edge features
+    if len(mlp_pred) == len(set(mlp_pred)):
+        cx, depth = get_transpiled_circ_results(qc, backend, initial_layout=mlp_pred)
+        mlp_depth.append(depth)
+        mlp_CNOT.append(cx)
+    else:
+        mlp_depth.append(float('NaN'))
+        mlp_CNOT.append(float('NaN'))
+        
+    # DNN without repair operator
+    if len(pred_or) == len(set(pred_or)):
+        cx, depth = get_transpiled_circ_results(qc, backend, initial_layout=pred_or)
+        dnn_without_ro_depth.append(depth)
+        dnn_without_ro_CNOT.append(cx)
+    else:
+        dnn_without_ro_depth.append(float("NaN"))
+        dnn_without_ro_CNOT.append(float("NaN"))
+        
+    # DNN with RO
+    cx, depth = get_transpiled_circ_results(qc, backend, initial_layout=pred_nr)
+    dnn_with_ro_depth.append(depth)
+    dnn_with_ro_CNOT.append(cx)
 print("DNN accuracy without repair operator:", count_or/len(test_dataset))
 print("DNN accuracy with repair operator:", count_nr/len(test_dataset))
 print("MLP accuracy:", count_mlp/len(test_dataset))
 print("MLP with edge features accuracy:", count_mlp_edge/len(test_dataset))
+
+df = pd.DataFrame()
+df["Label Depth"] = original_depth
+df["Label CNOTs"] = original_CNOT
+
+df["NN without edge features Depth"] = mlp_depth
+df["NN without edge features CNOTs"] = mlp_CNOT
+
+df["NN with edge features Depth"] = mlp_edge_depth
+df["NN with edge features CNOTs"] = mlp_edge_CNOT
+
+df["DNN without repair Depth"] = dnn_without_ro_depth
+df["DNN without repair CNOTs"] = dnn_without_ro_CNOT
+
+df["DNN with repair Depth"] = dnn_with_ro_depth
+df["DNN with repair CNOTs"] = dnn_with_ro_CNOT
+
+display(df)
+df.to_csv("models/test_dataset_result.csv")
 
